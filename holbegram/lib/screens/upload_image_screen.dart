4ff15d1e74f73460d145ca44bda9f59b93f08f4d
Hana Ouerghemmi <5004@holbertonstudents.com>
2023-10-31 10:25:00 +0100

    holbegram

---
+++ holbegram/lib/screens/upload_image_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UploadImage extends StatelessWidget {
+  const UploadImage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
